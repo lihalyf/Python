54267789225a76be1c6da63ec65f2b27ddc86d4f
lihalyf <lihanooo@hotmail.com>
2018-10-23 14:16:41 -0700

    Update All_Subsets(Lintcode_17).py

--- All_Subsets(Lintcode_17).py
+++ All_Subsets(Lintcode_17).py
@@ -1,3 +1,4 @@
+#version 1 (0, 1 select or not)
 import copy
 def subsets(nums):
     result = []
@@ -27,4 +28,30 @@ def dfs(num_list, index, subset, result): # 1. recursion define
 
 n = [3,1,2]
 r = subsets(n)
+print(r)
+
+#version 2
+
+def Subsets(nums):
+    result = []
+    if nums == None:
+        return result
+    
+    nums = sorted(nums)
+    dfs(nums, 0, [], result)
+    return result
+    
+def dfs(num, startIndex, subset, result):
+    result.append(list(subset))
+    
+    while startIndex < len(num):
+        subset.append(num[startIndex])
+        dfs(num, startIndex + 1, subset, result)
+        subset.pop()
+        startIndex += 1
+    
+    return result
+
+n = [3,1,2]
+r = Subsets(n)
 print(r)
