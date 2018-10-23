2c70fb21d1db8bb033f52c83d19dc41ce5744b15
lihalyf <lihanooo@hotmail.com>
2018-10-23 11:43:53 -0700

    Update All_Subsets(Lintcode_17).py

---
+++ All_Subsets(Lintcode_17).py
@@ -0,0 +1,30 @@
+import copy
+def subsets(nums):
+    result = []
+    if nums == None:
+        return result
+    
+    subset = []
+    nums = sorted(nums)
+    dfs(nums, 0, subset, result)
+    return result
+
+
+def dfs(num_list, index, subset, result): # 1. recursion define
+    # 2. recursion out
+    if index == len(num_list):
+        result = result.append(copy.deepcopy(subset)) #deep copy!!! so the result and subset uses different memory to store
+        return result
+    
+    # 3. recursion divide
+    ## select current index
+    subset.append(num_list[index])
+    dfs(num_list, index + 1, subset, result)
+    
+    ## not select current index
+    subset.pop()
+    dfs(num_list, index + 1, subset, result)
+
+n = [3,1,2]
+r = subsets(n)
+print(r)
