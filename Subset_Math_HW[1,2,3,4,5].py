7996febbf7140863872fdadb824c57518b12b927
lihalyf <lihanooo@hotmail.com>
2018-10-24 14:53:10 -0700

    Update Subset_Math_HW[1,2,3,4,5].py
    Find 5 elements subsets which does not contain the same number at the same position.

--- Subset_Math_HW[1,2,3,4,5].py
+++ Subset_Math_HW[1,2,3,4,5].py
@@ -1,17 +1,17 @@
 def hw4():
     nums = [1, 2, 3, 4, 5]
     result = []
-    dfs(nums, 5, 1, [], result)
+    dfs(nums, len(nums), 1, [], result)
     return len(result)
     
 def dfs(S, k, level, subset, result):
-    if k == 0:
+    if level > k:  # level is the number of element in subset, return result when level is bigger than len(nums)
         return result.append(list(subset))   
     for i in range(len(S)):
         if S[i] in subset or level == S[i]:
             continue
         subset.append(S[i])
-        dfs(S, k - 1, level + 1, subset, result)
+        dfs(S, k, level + 1, subset, result)
         subset.pop()
 r = hw4()
 print(r)
